c04208f133c15829c56d17781ecc80d411d06d23
Yinger90@gmail.com <Yinger90@gmail.com@c784204a-0df3-370e-24e1-4ebcab5a5b76>
2010-10-21 10:14:11 +0000

    New Kitchen GUI

--- trunk/Restaurant Ordering System/src/RestOrdSystem/Kitchen.java
+++ trunk/Restaurant Ordering System/src/RestOrdSystem/Kitchen.java
@@ -1,107 +1,120 @@
 package RestOrdSystem;
-
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JList;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JRadioButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.ButtonGroup;
-import javax.swing.JLabel;
-import java.awt.Font;
 import javax.swing.JButton;
-import java.awt.Color;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * KitchenGUI.java
+ *
+ * Created on Oct 21, 2010, 4:27:05 PM
+ */
+
+/**
+ *
+ * @author Yinger
+ */
+public class Kitchen extends javax.swing.JFrame {
+
+    /** Creates new form KitchenGUI */
+    public Kitchen() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        jLayeredPane1 = new javax.swing.JLayeredPane();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximizedBounds(new java.awt.Rectangle(0, 0, 800, 600));
+        setMinimumSize(new java.awt.Dimension(800, 600));
+        getContentPane().setLayout(null);
+
+        jLayeredPane1.setForeground(new java.awt.Color(51, 51, 51));
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 18));
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Kitchen");
+        jLabel2.setBounds(20, 20, 100, 30);
+        jLayeredPane1.add(jLabel2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jScrollPane1.setBounds(20, 70, 550, 400);
+        jLayeredPane1.add(jScrollPane1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        jButton1.setText("Served");
+        jButton1.setBounds(40, 420, 80, -1);
+        jLayeredPane1.add(jButton1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        getContentPane().add(jLayeredPane1);
+        jLayeredPane1.setBounds(0, 0, 800, 600);
+        
+        btnServed = new JButton("Served");
+        btnServed.setBounds(40, 500, 89, 23);
+        jLayeredPane1.add(btnServed);
+                
+                rdbtnPending = new JRadioButton("Pending");
+                buttonGroup.add(rdbtnPending);
+                rdbtnPending.setBounds(617, 119, 109, 23);
+                jLayeredPane1.add(rdbtnPending);
+                
+                rdbtnInProgress = new JRadioButton("In Progress");
+                buttonGroup.add(rdbtnInProgress);
+                rdbtnInProgress.setBounds(617, 145, 109, 23);
+                jLayeredPane1.add(rdbtnInProgress);
+                
+                rdbtnDone = new JRadioButton("Done");
+                buttonGroup.add(rdbtnDone);
+                rdbtnDone.setBounds(617, 171, 109, 23);
+                jLayeredPane1.add(rdbtnDone);
+                jLabel1 = new javax.swing.JLabel();
+                
+                        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Yinger\\Desktop\\4033133321_4cf4012508_o.png")); // NOI18N
+                        jLabel1.setText("jLabel1");
+                        jLabel1.setBounds(0, 0, 800, 600);
+                        jLayeredPane1.add(jLabel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        pack();
+    }
+    
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Kitchen().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLayeredPane jLayeredPane1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private JButton btnServed;
+    private JRadioButton rdbtnPending;
+    private JRadioButton rdbtnInProgress;
+    private JRadioButton rdbtnDone;
+    private final ButtonGroup buttonGroup = new ButtonGroup();
 
 
-public class Kitchen extends JFrame {
-
-	private JPanel contentPane;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Kitchen frame = new Kitchen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Kitchen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(186, 85, 211));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JList list = new JList();
-		
-		JLabel label = new JLabel("Kitchen");
-		label.setFont(new Font("Traditional Arabic", Font.BOLD, 18));
-		
-		JRadioButton Inprogress = new JRadioButton("In progress");
-		Inprogress.setBackground(new Color(255, 69, 0));
-		buttonGroup.add(Inprogress);
-		JRadioButton Done = new JRadioButton("Done");
-		Done.setBackground(new Color(173, 255, 47));
-		buttonGroup.add(Done);
-		JRadioButton Delivered = new JRadioButton("Delivered");
-		Delivered.setBackground(new Color(240, 255, 240));
-		buttonGroup.add(Delivered);
-		
-		JButton button = new JButton("Remove");
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(5)
-							.addComponent(label, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(list, GroupLayout.PREFERRED_SIZE, 504, GroupLayout.PREFERRED_SIZE)
-							.addGap(37)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGap(5)
-									.addComponent(button, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE))
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(Inprogress, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-									.addComponent(Done, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(Delivered, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-					.addGap(214))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(list, GroupLayout.PREFERRED_SIZE, 444, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(Inprogress)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(Done)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(Delivered)
-							.addGap(84)
-							.addComponent(button)))
-					.addContainerGap(12, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
-	}
 }
