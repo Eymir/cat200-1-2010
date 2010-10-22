07d2c597f30b0b23c9b156879c435906937e7dfc
douglashong3@gmail.com <douglashong3@gmail.com@c784204a-0df3-370e-24e1-4ebcab5a5b76>
2010-10-22 07:54:01 +0000

    Updated Cashier GUI with Table and some Layout

--- trunk/Restaurant Ordering System/src/RestOrdSystem/CashierModule.java
+++ trunk/Restaurant Ordering System/src/RestOrdSystem/CashierModule.java
@@ -4,14 +4,24 @@ package RestOrdSystem;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.TitledBorder;
 
 
 public class CashierModule extends JFrame {
 
-	static public String [] TableStatus; 
+	static public String [] TableStatus;
+	static public int TableNumber; 
 	
 	
-	private JPanel BasePanel;
+	private JTable BillTable;
+	
+	
+	private JScrollPane BillTableScrollPane;
+	
+	
+	private JPanel BasePanel,ButtonPanel;
+	
 	
 	private JToggleButton Table1Button,Table2Button,Table3Button,Table4Button,Table5Button,
 						  Table6Button,Table7Button,Table8Button,Table9Button,Table10Button,
@@ -27,7 +37,6 @@ public class CashierModule extends JFrame {
 	
 	public CashierModule() {
         initComponents();
-        setLocationRelativeTo(null);
     }
     
 	
@@ -40,12 +49,21 @@ public class CashierModule extends JFrame {
     }
     
     
-    private void initComponents() {
+    @SuppressWarnings("serial")
+	private void initComponents() {
     	 
         TableStatus = new String[20];
+        TableNumber = 1;
+        
+        
+        BillTableScrollPane = new JScrollPane();
+        
+        
+        BillTable = new JTable();
         
         
         BasePanel = new JPanel();
+        ButtonPanel = new JPanel();
         
         
     	Table1Button = new JToggleButton();
@@ -102,60 +120,137 @@ public class CashierModule extends JFrame {
         setResizable(false);
         setMinimumSize(new Dimension(800,600));
         getContentPane().setLayout(null);
-        
+        this.setLocationRelativeTo(null);
         
         getContentPane().add(BasePanel);
         BasePanel.setLayout(null);
-        BasePanel.setBounds(0,0,794,572);
-        BasePanel.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new Color(0, 0, 0)));
+        BasePanel.setBounds(0,0,798,568);
+        BasePanel.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, new Color(0, 0, 0)));
         BasePanel.setBackground(new Color(51,51,51));
               
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////Layout Ordering/////////////////////////////////////////////////////////////////////////////////////////////////        
         
-        BasePanel.add(Table1Button);
-        BasePanel.add(Table2Button);
-        BasePanel.add(Table3Button);
-        BasePanel.add(Table4Button);
-        BasePanel.add(Table5Button);
-        BasePanel.add(Table6Button);
-        BasePanel.add(Table7Button);
-        BasePanel.add(Table8Button);
-        BasePanel.add(Table9Button);
-        BasePanel.add(Table10Button);
-        BasePanel.add(Table11Button);
-        BasePanel.add(Table12Button);
-        BasePanel.add(Table13Button);
-        BasePanel.add(Table14Button);
-        BasePanel.add(Table15Button);
-        BasePanel.add(Table16Button);
-        BasePanel.add(Table17Button);
-        BasePanel.add(Table18Button);
-        BasePanel.add(Table19Button);
-        BasePanel.add(Table20Button);
+        BasePanel.add(BillTableScrollPane);
+        BasePanel.add(ButtonPanel);
         
         
-        BasePanel.add(Table1Label);
-        BasePanel.add(Table2Label);
-        BasePanel.add(Table3Label);
-        BasePanel.add(Table4Label);
-        BasePanel.add(Table5Label);
-        BasePanel.add(Table6Label);
-        BasePanel.add(Table7Label);
-        BasePanel.add(Table8Label);
-        BasePanel.add(Table9Label);
-        BasePanel.add(Table10Label);
-        BasePanel.add(Table11Label);
-        BasePanel.add(Table12Label);
-        BasePanel.add(Table13Label);
-        BasePanel.add(Table14Label);
-        BasePanel.add(Table15Label);
-        BasePanel.add(Table16Label);
-        BasePanel.add(Table17Label);
-        BasePanel.add(Table18Label);
-        BasePanel.add(Table19Label);
-        BasePanel.add(Table20Label);
+        BillTableScrollPane.add(BillTable);
         
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        ButtonPanel.add(Table1Button);
+        ButtonPanel.add(Table2Button);
+        ButtonPanel.add(Table3Button);
+        ButtonPanel.add(Table4Button);
+        ButtonPanel.add(Table5Button);
+        ButtonPanel.add(Table6Button);
+        ButtonPanel.add(Table7Button);
+        ButtonPanel.add(Table8Button);
+        ButtonPanel.add(Table9Button);
+        ButtonPanel.add(Table10Button);
+        ButtonPanel.add(Table11Button);
+        ButtonPanel.add(Table12Button);
+        ButtonPanel.add(Table13Button);
+        ButtonPanel.add(Table14Button);
+        ButtonPanel.add(Table15Button);
+        ButtonPanel.add(Table16Button);
+        ButtonPanel.add(Table17Button);
+        ButtonPanel.add(Table18Button);
+        ButtonPanel.add(Table19Button);
+        ButtonPanel.add(Table20Button);
+        
+        
+        ButtonPanel.add(Table1Label);
+        ButtonPanel.add(Table2Label);
+        ButtonPanel.add(Table3Label);
+        ButtonPanel.add(Table4Label);
+        ButtonPanel.add(Table5Label);
+        ButtonPanel.add(Table6Label);
+        ButtonPanel.add(Table7Label);
+        ButtonPanel.add(Table8Label);
+        ButtonPanel.add(Table9Label);
+        ButtonPanel.add(Table10Label);
+        ButtonPanel.add(Table11Label);
+        ButtonPanel.add(Table12Label);
+        ButtonPanel.add(Table13Label);
+        ButtonPanel.add(Table14Label);
+        ButtonPanel.add(Table15Label);
+        ButtonPanel.add(Table16Label);
+        ButtonPanel.add(Table17Label);
+        ButtonPanel.add(Table18Label);
+        ButtonPanel.add(Table19Label);
+        ButtonPanel.add(Table20Label);
+        
+ ///////Button Panel Layout//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        ButtonPanel.setLayout(null);
+        ButtonPanel.setBounds(10,10,480,520);
+        ButtonPanel.setBackground(new Color(51,51,51));
+        ButtonPanel.setBorder(BorderFactory.createTitledBorder(null,"Table "+TableNumber+"'s Bill",TitledBorder.DEFAULT_JUSTIFICATION,TitledBorder.DEFAULT_POSITION, new Font("Dialog", 1, 12),new Color(220,220,220)));
+        
+        
+////////Bill Table with Scroll Pane///////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        BillTableScrollPane.setBounds(495,10,285,456);
+        BillTable.setModel(new DefaultTableModel(
+                new Object [][] {
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null}, 
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+
+                },
+                new String [] {
+                    "No.", "Item", "Unit", "Price"
+                }
+            ) {
+
+				boolean[] canEdit = new boolean [] {
+                    false, false, false, false
+                };
+
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    return canEdit [columnIndex];
+                }
+            });
+        
+        BillTableScrollPane.setViewportView(BillTable);
+        BillTable.getColumnModel().getColumn(0).setResizable(false);
+        BillTable.getColumnModel().getColumn(0).setPreferredWidth(30);
+        BillTable.getColumnModel().getColumn(1).setResizable(false);
+        BillTable.getColumnModel().getColumn(1).setPreferredWidth(150);
+        BillTable.getColumnModel().getColumn(2).setResizable(false);
+        BillTable.getColumnModel().getColumn(2).setPreferredWidth(40);
+        BillTable.getColumnModel().getColumn(3).setResizable(false);
+        BillTable.getColumnModel().getColumn(3).setPreferredWidth(60);
+       
+        BillTableScrollPane.setBackground(new Color(51,51,51));
+        BillTableScrollPane.setBorder(BorderFactory.createTitledBorder(null,"Table "+TableNumber+"'s Bill",TitledBorder.DEFAULT_JUSTIFICATION,TitledBorder.DEFAULT_POSITION, new Font("Dialog", 1, 12),new Color(220,220,220)));
+     
+        
+        
+////////Table Label//////////////////////////////////////////////////////////////////////////////////////////////////////
         
         Table1Label.setHorizontalAlignment(SwingConstants.CENTER);
         Table2Label.setHorizontalAlignment(SwingConstants.CENTER);
@@ -245,28 +340,28 @@ public class CashierModule extends JFrame {
         Table20Label.setForeground(new Color(220,220,220));
         
         
-        Table1Label.setBounds(40,47,200,46);
-        Table2Label.setBounds(40,97,200,46);
-        Table3Label.setBounds(40,147,200,46);
-        Table4Label.setBounds(40,197,200,46);
-        Table5Label.setBounds(40,247,200,46);
-        Table6Label.setBounds(40,297,200,46);
-        Table7Label.setBounds(40,347,200,46);
-        Table8Label.setBounds(40,397,200,46);
-        Table9Label.setBounds(40,447,200,46);
-        Table10Label.setBounds(40,497,200,46);
-        Table11Label.setBounds(280,47,200,46);
-        Table12Label.setBounds(280,97,200,46);
-        Table13Label.setBounds(280,147,200,46);
-        Table14Label.setBounds(280,197,200,46);
-        Table15Label.setBounds(280,247,200,46);
-        Table16Label.setBounds(280,297,200,46);
-        Table17Label.setBounds(280,347,200,46);
-        Table18Label.setBounds(280,397,200,46);
-        Table19Label.setBounds(280,447,200,46);
-        Table20Label.setBounds(280,497,200,46);
+        Table1Label.setBounds(30,17,200,46);
+        Table2Label.setBounds(30,67,200,46);
+        Table3Label.setBounds(30,117,200,46);
+        Table4Label.setBounds(30,167,200,46);
+        Table5Label.setBounds(30,217,200,46);
+        Table6Label.setBounds(30,267,200,46);
+        Table7Label.setBounds(30,317,200,46);
+        Table8Label.setBounds(30,367,200,46);
+        Table9Label.setBounds(30,417,200,46);
+        Table10Label.setBounds(30,467,200,46);
+        Table11Label.setBounds(270,17,200,46);
+        Table12Label.setBounds(270,67,200,46);
+        Table13Label.setBounds(270,117,200,46);
+        Table14Label.setBounds(270,167,200,46);
+        Table15Label.setBounds(270,217,200,46);
+        Table16Label.setBounds(270,267,200,46);
+        Table17Label.setBounds(270,317,200,46);
+        Table18Label.setBounds(270,367,200,46);
+        Table19Label.setBounds(270,417,200,46);
+        Table20Label.setBounds(270,467,200,46);
         
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////Table Button//////////////////////////////////////////////////////////////////////////////////////////
         
         Table1Button.setText("Table 1");
         Table2Button.setText("Table 2");
@@ -290,26 +385,26 @@ public class CashierModule extends JFrame {
         Table20Button.setText("Table 20");
                
         
-        Table1Button.setBounds(20,50,100,40);
-        Table2Button.setBounds(20,100,100,40);
-        Table3Button.setBounds(20,150,100,40);
-        Table4Button.setBounds(20,200,100,40);
-        Table5Button.setBounds(20,250,100,40);
-        Table6Button.setBounds(20,300,100,40);
-        Table7Button.setBounds(20,350,100,40);
-        Table8Button.setBounds(20,400,100,40);
-        Table9Button.setBounds(20,450,100,40);
-        Table10Button.setBounds(20,500,100,40);
-        Table11Button.setBounds(260,50,100,40);
-        Table12Button.setBounds(260,100,100,40);
-        Table13Button.setBounds(260,150,100,40);
-        Table14Button.setBounds(260,200,100,40);
-        Table15Button.setBounds(260,250,100,40);
-        Table16Button.setBounds(260,300,100,40);
-        Table17Button.setBounds(260,350,100,40);
-        Table18Button.setBounds(260,400,100,40);
-        Table19Button.setBounds(260,450,100,40);
-        Table20Button.setBounds(260,500,100,40);
+        Table1Button.setBounds(10,20,100,40);
+        Table2Button.setBounds(10,70,100,40);
+        Table3Button.setBounds(10,120,100,40);
+        Table4Button.setBounds(10,170,100,40);
+        Table5Button.setBounds(10,220,100,40);
+        Table6Button.setBounds(10,270,100,40);
+        Table7Button.setBounds(10,320,100,40);
+        Table8Button.setBounds(10,370,100,40);
+        Table9Button.setBounds(10,420,100,40);
+        Table10Button.setBounds(10,470,100,40);
+        Table11Button.setBounds(250,20,100,40);
+        Table12Button.setBounds(250,70,100,40);
+        Table13Button.setBounds(250,120,100,40);
+        Table14Button.setBounds(250,170,100,40);
+        Table15Button.setBounds(250,220,100,40);
+        Table16Button.setBounds(250,270,100,40);
+        Table17Button.setBounds(250,320,100,40);
+        Table18Button.setBounds(250,370,100,40);
+        Table19Button.setBounds(250,420,100,40);
+        Table20Button.setBounds(250,470,100,40);
            
  /////////////////////////////////////////////////////////////////////////////////////////////////////////      
         pack();
