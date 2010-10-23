776d49b9d2f1d4418596b48d0ab397e4c470a804
yinger90 <Yinger90@gmail.com>
2010-10-23 11:39:44 +0000

    Kitchen Module basically done.

--- Restaurant Ordering System/src/RestOrdSystem/Kitchen.java
+++ Restaurant Ordering System/src/RestOrdSystem/Kitchen.java
@@ -6,9 +6,17 @@ import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.*;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Kitchen extends javax.swing.JFrame {
 
@@ -17,6 +25,11 @@ public class Kitchen extends javax.swing.JFrame {
         setTitle("Kitchen");
         setResizable(false);
         setLocationRelativeTo(null);
+        tableview();
+        btnServed.setEnabled(false);
+        rdbtnPending.setEnabled(false);
+        rdbtnInProgress.setEnabled(false);
+        rdbtnDone.setEnabled(false);
     }
 
     private void initComponents() {
@@ -52,6 +65,7 @@ public class Kitchen extends javax.swing.JFrame {
                 "Table Number", "Food", "Quantity", "Status"
             }
         ));
+        jTable1.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(jTable1);
 
         jScrollPane1.setBounds(20, 189, 550, 281);
@@ -65,55 +79,86 @@ public class Kitchen extends javax.swing.JFrame {
         jLayeredPane1.setBounds(0, 0, 800, 600);
         
         btnServed = new JButton("Served");
-        btnServed.addMouseListener(new MouseAdapter() {
-        	public void mouseClicked(MouseEvent arg0) {
+        btnServed.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		
         		delete();
+        		tableview();
+            	
         	}
         });
+        
         btnServed.setBounds(40, 500, 89, 23);
         jLayeredPane1.add(btnServed);
                 
         rdbtnPending = new JRadioButton("Pending");
-        rdbtnPending.addMouseListener(new MouseAdapter() {
-        	public void mouseClicked(MouseEvent arg0) {
+        rdbtnPending.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		status = "Pending";
         		warn();
+        		tableview();
         	}
         });
+        
         buttonGroup.add(rdbtnPending);
         rdbtnPending.setBounds(617, 192, 109, 23);
         jLayeredPane1.add(rdbtnPending);
                 
         rdbtnInProgress = new JRadioButton("In Progress");
-        rdbtnInProgress.addMouseListener(new MouseAdapter() {
-        	public void mouseClicked(MouseEvent arg0) {
+        rdbtnInProgress.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		status = "In Progress";
         		warn();
+        		tableview();
         	}
         });
+        
         buttonGroup.add(rdbtnInProgress);
         rdbtnInProgress.setBounds(617, 218, 109, 23);
         jLayeredPane1.add(rdbtnInProgress);
                 
         rdbtnDone = new JRadioButton("Done");
-        rdbtnDone.addMouseListener(new MouseAdapter() {
-        	public void mouseClicked(MouseEvent arg0) {
+        rdbtnDone.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		status = "Done";
         		warn();
+        		tableview();
         	}
         });
+        
         buttonGroup.add(rdbtnDone);
         rdbtnDone.setBounds(617, 244, 109, 23);
         jLayeredPane1.add(rdbtnDone);
         
-        JLabel label = new JLabel("New label");
-        label.addMouseListener(new MouseAdapter() {
-        	public void mouseClicked(MouseEvent arg0) {
-        		
+        JButton button = new JButton("");
+        button.setIcon(new ImageIcon(Kitchen.class.getResource("/image1/Symbol Black Refresh.jpg")));
+        button.setBounds(520, 114, 50, 50);
+        jLayeredPane1.add(button);
+        
+        lblRefresh = new JLabel("Refresh");
+        button.addMouseListener(new MouseAdapter() {
+        	public void mouseEntered(MouseEvent arg0) {
+        		lblRefresh.setForeground(Color.YELLOW);
+        	}
+        	public void mouseExited(MouseEvent e) {
+        		lblRefresh.setForeground(Color.WHITE);
+        	}
+        	public void mouseClicked(MouseEvent e) {
+        		tableview();
+        		btnServed.setEnabled(false);
+                rdbtnPending.setEnabled(false);
+                rdbtnInProgress.setEnabled(false);
+                rdbtnDone.setEnabled(false);
         	}
         });
-        label.setIcon(new ImageIcon(Kitchen.class.getResource("/image1/Symbol Black Refresh.jpg")));
-        label.setBounds(520, 128, 50, 50);
-        jLayeredPane1.add(label);
+        lblRefresh.setForeground(Color.WHITE);
+        lblRefresh.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblRefresh.setBounds(515, 165, 60, 20);
+        jLayeredPane1.add(lblRefresh);
         jLabel1 = new javax.swing.JLabel();
-                
+        jLabel1.setForeground(Color.BLACK);
+        jLabel1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+        
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image1/background.png"))); 
         jLabel1.setText("jLabel1");
         jLabel1.setBounds(0, 0, 800, 600);
@@ -124,7 +169,111 @@ public class Kitchen extends javax.swing.JFrame {
     
     Object[] confirm = {"Yes","No"};
     Object[] ok = {"ok"};
+    String status;
+    
+    int countRow = 0;
+    int countRow2 = 0;
+    int countColumn = 0;
+    int countColumn2 = 0;
+    int count = 0;
+    
     TableModel newtable1 = new TableModel();
+    
+    public void tableview(){
+    	countRow = 0;
+        countRow2 = 0;
+        countColumn = 0;
+        countColumn2 = 0;
+        count = 0;
+        
+        jTable1.repaint();
+        Dimension THIS_SIZE = (jLayeredPane1.getSize());
+        
+        jTable1.setMaximumSize(THIS_SIZE);
+        jTable1.setMinimumSize(THIS_SIZE);
+        jTable1.setPreferredSize(THIS_SIZE);
+        
+        try{
+        	DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
+
+            Connection con;
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hihi", "hihi");
+            System.out.println("Database Connected");
+        	
+        	Statement stmt = con.createStatement();
+
+            String queryString = "Select* From KITCHENTABLE";
+
+            ResultSet rset = stmt.executeQuery(queryString);
+
+            while (rset.next()){
+                countRow++;
+            }
+
+            ResultSetMetaData metadata = rset.getMetaData();
+            countColumn = metadata.getColumnCount();
+
+            newtable1.setDataSize(countRow, countColumn);
+            newtable1.setColumnNameSize();
+            //end
+
+            //adding column names to table model
+            DatabaseMetaData meta = con.getMetaData();
+
+            ResultSet rset2 = meta.getColumns(null, null, "KITCHENTABLE", null);
+            
+            	newtable1.setColumnName(0, "Order Number");
+                newtable1.setColumnName(1, "Table");
+                newtable1.setColumnName(2, "Food");
+                newtable1.setColumnName(3, "Quantity");
+                newtable1.setColumnName(4, "Status");
+            
+            
+            count = 0;
+            // end
+
+            rset = stmt.executeQuery(queryString);
+            while(rset.next()){
+            	newtable1.addString(countRow2, countColumn2, rset.getString("KIT_ORD_NO"));
+                countColumn2++;
+                newtable1.addString(countRow2, countColumn2, rset.getString("TABLE_NO"));
+                countColumn2++;
+                newtable1.addString(countRow2, countColumn2, rset.getString("FOOD_NAME"));
+                countColumn2++;
+                newtable1.addString(countRow2, countColumn2, rset.getString("FOOD_QN"));
+                countColumn2++;
+                newtable1.addString(countRow2, countColumn2, rset.getString("FOOD_STATUS"));
+                countColumn2++;
+
+                countColumn2 = 0;
+                countRow2++;
+            }
+        }
+
+        catch (Exception e){
+            System.out.println("error");
+        }
+        
+        this.setTable();
+        
+        ListSelectionModel selectionModel = jTable1.getSelectionModel();
+        selectionModel.addListSelectionListener(new ListSelectionListener(){
+
+            public void valueChanged(ListSelectionEvent e) {
+
+            	status=(String) newtable1.getValueAt(jTable1.getSelectedRow(), 4);
+            	if(status.equals("Done"))
+                btnServed.setEnabled(true);
+            	else
+            	btnServed.setEnabled(false);
+                rdbtnPending.setEnabled(true);
+                rdbtnInProgress.setEnabled(true);
+                rdbtnDone.setEnabled(true);
+            
+        }
+        }
+        ); 
+    }
     
     public void setTable(){
     	jTable1.setModel(newtable1);
@@ -133,11 +282,11 @@ public class Kitchen extends javax.swing.JFrame {
     public void update(String edit){
 
         try {
-            //DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
+            DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
 
             Connection con;
             con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hihi", "hihi");
-            System.out.println("Database Connected1");
+            System.out.println("Database Connected 1");
 
             Statement stmt = con.createStatement();
             System.out.println("success2");
@@ -158,8 +307,12 @@ public class Kitchen extends javax.swing.JFrame {
     	int n = warnMessage();
 
         if(n == 0){
-        
-        	
+        	String no= (String) newtable1.getValueAt(jTable1.getSelectedRow(), 0);
+        	String table = (String) newtable1.getValueAt(jTable1.getSelectedRow(), 1);
+        	String updateString = "UPDATE KITCHENTABLE SET FOOD_STATUS='"+status+"' WHERE KIT_ORD_NO = '"+no+"'AND FOOD_NAME='"+table+"'";
+
+            update(updateString);
+            btnServed.setEnabled(false);
         }
     }
     
@@ -169,7 +322,11 @@ public class Kitchen extends javax.swing.JFrame {
 
         if(n == 0){
         
-        	
+        	String no= (String) newtable1.getValueAt(jTable1.getSelectedRow(), 0);
+        	String table = (String) newtable1.getValueAt(jTable1.getSelectedRow(), 1);
+        	String deleteString = "DELETE FROM KITCHENTABLE WHERE KIT_ORD_NO = '"+no+"'AND FOOD_NAME='"+table+"'";
+
+            update(deleteString);
         }
     }
     
@@ -224,5 +381,6 @@ public class Kitchen extends javax.swing.JFrame {
     private JRadioButton rdbtnPending;
     private JRadioButton rdbtnInProgress;
     private JRadioButton rdbtnDone;
+    private javax.swing.JLabel lblRefresh;
     private final ButtonGroup buttonGroup = new ButtonGroup();
 }
