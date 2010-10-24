87e2c201a63452d520c8c6917e29c4c22b9f38c4
diligentlyshane@gmail.com <diligentlyshane@gmail.com@c784204a-0df3-370e-24e1-4ebcab5a5b76>
2010-10-24 11:38:33 +0000

    finish modify food JTable function

--- trunk/Restaurant Ordering System/src/RestOrdSystem/Administrator/FoodBusiness.java
+++ trunk/Restaurant Ordering System/src/RestOrdSystem/Administrator/FoodBusiness.java
@@ -72,5 +72,52 @@ public class FoodBusiness {
 		
 		
 	}
+	
+	public boolean updatePicture(FoddModel food){
+		boolean flag=false;
+		
+		String sql="update FOODTABLE set FOOD_PICT_URL=? where lower(FOOD_NAME)=lower(?)";
+		try {
+			pst=new DBAccess().connOracle(sql);
+			pst.setString(1,food.getPicture());
+			pst.setString(2,food.getFoodName());
+			if(pst.executeUpdate()>0){
+				flag=true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+		
+	}
+	
+	public boolean updateOtherInfo(FoddModel food){
+		boolean flag=false;
+		String sql="update FOODTABLE set FOOD_TYPE =? ,FOOD_PRICE =?,FOOD_QOH =?,FOOD_MIN =?,FOOD_DESCPT =?  where lower(FOOD_NAME)=lower(?)";
+		
+		try {
+			pst=new DBAccess().connOracle(sql);
+			pst.setString(1, food.getFoodType());
+			pst.setInt(2,food.getFoodPrice());
+			pst.setInt(3,food.getFoodQoh());
+			pst.setInt(4,food.getFoodMin());
+			pst.setString(5,food.getDescription());
+			pst.setString(6,food.getFoodName());
+			if(pst.executeUpdate()>0){
+				flag=true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return flag;
+	}
 
 }
